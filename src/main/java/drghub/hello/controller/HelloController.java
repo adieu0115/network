2cd6c2f86f5e2f8e39e6c9b23321acaabc2dddac
doubleDRG <dyl660115@gmail.com>
2024-10-22 13:03:21 +0900

    change messgae

--- src/main/java/drghub/hello/controller/HelloController.java
+++ src/main/java/drghub/hello/controller/HelloController.java
@@ -6,9 +6,11 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController("/")
 public class HelloController
 {
+    static final String message = "그만 놀고 공부해라.";
+
     @RequestMapping("/hello")
     public String hello()
     {
-        return "hello world";
+        return message;
     }
 }
